afc09dc736a6b293a291bfac9c2352d8400e0d18
kjkondo <44207296+kjkondo@users.noreply.github.com>
2020-07-30 21:45:06 -0700

    fix insta link

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,7 +4,7 @@
       Contact
     </h2>
     <p align="center" justify="center">
-      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><v-img alt="Instagram Logo" height='100' width ='100'  contain  :src="require('@/assets/insta.png')"/></a>
+      <a href="https://www.instagram.com/cppsupermileage/"><v-img alt="Instagram Logo" height='100' width ='100'  contain  :src="require('@/assets/insta.png')"/></a>
     </p>
   </v-container>
 </template>
